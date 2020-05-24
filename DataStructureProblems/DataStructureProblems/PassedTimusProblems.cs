90551eaa07c3ba1badc65131e2c17842046501e0
Hakobyan888 <56021294+Hakobyan888@users.noreply.github.com>
2020-05-24 10:47:52 +0400

    new timus problems

--- DataStructureProblems/DataStructureProblems/PassedTimusProblems.cs
+++ DataStructureProblems/DataStructureProblems/PassedTimusProblems.cs
@@ -475,22 +475,6 @@ namespace DataStructureProblems
 
         static void Timus1353()
         {
-            //int[] ways = new int[1000];
-            //long sum = 0;
-            //long s = long.Parse(Console.ReadLine());
-            //long n = (long)Math.Pow(10, 9);
-            //int i = 1;
-            //if (s == 1) sum++;
-            //if (s > 69)
-            //{
-            //    i = 8;
-            //}
-            //for (int i = 0; i <= 10; i++)
-            //{
-            //    //sum += findCount(i, s);
-            //    dfs(i, s);
-            //}
-            //Console.WriteLine(count);
             var s = Convert.ToInt32(Console.ReadLine());
             var VF = new long[s + 1];
             VF[0] = 1;
@@ -508,24 +492,6 @@ namespace DataStructureProblems
         static int count = 0;
 
         public static long[] VF;
-        //public static long f(int N, long S)
-        //{
-        //    if (S < 0) return 0;
-        //    if (N == 0)
-        //        return S == 0 ? 1 : 0;
-        //    if (memo[N, S] != 0)
-        //        return memo[N, S];
-        //    long ans = 0;
-        //    for (int k = 0; k <= 9; k++)
-        //        ans += f(N - 1, S - k);
-        //    return memo[N, S] = ans;
-        //}
-
-        //public static long g(int N, long S)
-        //{
-        //    return f(N, S) - f(N - 1, S);
-        //}
-
         static void Timus2068()
         {
             int n = int.Parse(Console.ReadLine());
@@ -2189,18 +2155,6 @@ namespace DataStructureProblems
             Console.WriteLine(res);
         }
 
-        static void Timus1837()
-        {
-            int n = int.Parse(Console.ReadLine());
-            List<List<string>> groups = new List<List<string>>();
-            for (int i = 0; i < n; i++)
-            {
-                var value = new List<string>();
-                value.AddRange(Console.ReadLine().Split());
-                groups.Add(value);
-            }
-        }
-
         static void Timus1563()
         {
             int n = int.Parse(Console.ReadLine());
@@ -2398,21 +2352,6 @@ namespace DataStructureProblems
             Console.WriteLine(k.Last());
         }
 
-        static void Timus1152()
-        {
-
-        }
-
-        static void Timus1651()
-        {
-
-        }
-
-        static void Timus1133()
-        {
-
-        }
-
         static void Timus1518Working()
         {
             var nxy = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
@@ -2522,6 +2461,376 @@ namespace DataStructureProblems
                             max = Math.Max(sum, max);
                         }
             Console.WriteLine(max);
+        }
+
+
+        public static void Timus1462()
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var arr = new BigInteger[n + 1];
+            arr[0] = 4; // 3
+            arr[1] = 7; // 4
+            for (int i = 2; i <= n; i++)
+            {
+                arr[i] = BigInteger.Add(arr[i - 1], arr[i - 2]);
+                if (i > 4)
+                {
+                    arr[i - 4] = 0;
+                }
+            }
+            Console.WriteLine(arr[n - 3]);
+        }
+
+        public static void Timus1095()
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var m = Console.ReadLine();
+                Console.WriteLine(DivideSeven(m));
+            }
+        }
+        public static BigInteger DivideSeven(string m)
+        {
+            var nums = new[]
+            {
+                1234,
+                1243,
+                1324,
+                1342,
+                1423,
+                1432,
+                2134,
+                2143,
+                2341,
+                2314,
+                2413,
+                2431,
+                3124,
+                3142,
+                3214,
+                3241,
+                3421,
+                3412,
+                4123,
+                4132,
+                4213,
+                4231,
+                4312,
+                4321
+            };
+            var num = "";
+            var zeros = "";
+            var temp = "";
+            foreach (var c in m)
+            {
+                if (c == '1' && !temp.Contains("1"))
+                    temp += "1";
+                else if (c == '2' && !temp.Contains("2"))
+                    temp += "2";
+                else if (c == '3' && !temp.Contains("3"))
+                    temp += "3";
+                else if (c == '4' && !temp.Contains("4"))
+                    temp += "4";
+                else if (c == '0')
+                    zeros += c;
+                else
+                    num += c;
+            }
+            if (num == "" && zeros == "")
+            {
+                return 4123;
+            }
+            if (num == "" && zeros != "")
+            {
+                foreach (var s in nums)
+                {
+                    if (s * Math.Pow(10, zeros.Length) % 7 == 0)
+                    {
+                        return new BigInteger(s * Math.Pow(10, zeros.Length));
+                    }
+                }
+            }
+            var number = long.Parse(num + zeros);
+            var res = new BigInteger(0);
+            foreach (var s in nums)
+            {
+                res = BigInteger.Add(BigInteger.Multiply(number, 10000), s);
+                BigInteger r = BigInteger.One;
+                BigInteger.DivRem(res, new BigInteger(7), out r);
+                if (r == 0)
+                    break;
+            }
+            return res;
+        }
+
+
+        private static int n;
+        public static void Timus1033()
+        {
+            n = Convert.ToInt32(Console.ReadLine());
+            matrix = new int[n, n];
+            fill = new int[n, n];
+            for (var i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                for (var j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == '#')
+                        matrix[i, j] = 1;
+                }
+            }
+            FloodFill(0, 0);
+            FloodFill(n - 1, n - 1);
+            var ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (fill[i, j] != 1) continue;
+                    if (i + 1 > n - 1 || fill[i + 1, j] != 1)
+                        ans++;
+                    if (j + 1 > n - 1 || fill[i, j + 1] != 1)
+                        ans++;
+                    if (i - 1 < 0 || fill[i - 1, j] != 1)
+                        ans++;
+                    if (j - 1 < 0 || fill[i, j - 1] != 1)
+                        ans++;
+                }
+            }
+            Console.WriteLine((ans - 4) * 9);
+        }
+        private static int[,] fill;
+        private static int[,] matrix;
+        public static void FloodFill(int x, int y)
+        {
+            fill[x, y] = 1;
+            if (x + 1 < n && matrix[x + 1, y] != 1 && fill[x + 1, y] != 1)
+                FloodFill(x + 1, y);
+            if (y + 1 < n && matrix[x, y + 1] != 1 && fill[x, y + 1] != 1)
+                FloodFill(x, y + 1);
+            if (x - 1 >= 0 && matrix[x - 1, y] != 1 && fill[x - 1, y] != 1)
+                FloodFill(x - 1, y);
+            if (y - 1 >= 0 && matrix[x, y - 1] != 1 && fill[x, y - 1] != 1)
+                FloodFill(x, y - 1);
+        }
+
+        public static void Timus1160()
+        {
+            var nm = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = nm[0];
+            var m = nm[1];
+            var graph = new Graph<int>();
+            var graphAlgorithms = new GraphAlgorithms<int>();
+            for (int i = 1; i <= n; i++)
+                graph.AddVertexToList(i);
+            for (int i = 1; i <= m; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                graph.AddEdgeToList(Tuple.Create(input[0], input[1], (double)input[2]), true);
+            }
+
+            var answer = graphAlgorithms.Kruskal(graph);
+            var min = answer.Max().Weight;
+            var edgesCount = answer.Count;
+            Console.WriteLine(min);
+            Console.WriteLine(edgesCount);
+            foreach (var item in answer)
+            {
+                Console.WriteLine($"{item.EdgeFrom} {item.EdgeTo}");
+            }
+
+        }
+        public static void Timus1205()
+        {
+            var v1v2 = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var v1 = v1v2[0];
+            var v2 = v1v2[1];
+            var graph = new Graph<KeyValuePair<double, double>>();
+            var graphAlgorithms = new GraphAlgorithms<KeyValuePair<double, double>>();
+            var n = Convert.ToInt32(Console.ReadLine());
+            var points = new List<KeyValuePair<double, double>>();
+            var stationEdges = new List<Tuple<KeyValuePair<double, double>, KeyValuePair<double, double>>>();
+            for (int i = 0; i < n; i++)
+            {
+                var xy = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+                points.Add(new KeyValuePair<double, double>(xy[0], xy[1]));
+                graph.AddVertexToList(points[i]);
+            }
+            while (true)
+            {
+                var xy = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (xy[0] == 0 && xy[1] == 0)
+                    break;
+                var point1 = points[xy[0] - 1];
+                var point2 = points[xy[1] - 1];
+                stationEdges.Add(Tuple.Create(point1, point2));
+                graph.AddEdgeToList(Tuple.Create(point1, point2, Math.Round(Time(point1, point2, v2), 7)));
+            }
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    graph.AddEdgeToList(Tuple.Create(points[i], points[j], Math.Round(Time(points[i], points[j], v1), 7)));
+                }
+            }
+            var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var b = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var ap = new KeyValuePair<double, double>(a[0], a[1]);
+            var bp = new KeyValuePair<double, double>(b[0], b[1]);
+            graph.AddVertexToList(ap);
+            graph.AddVertexToList(bp);
+            graph.AddEdgeToList(Tuple.Create(ap, bp, Math.Round(Time(ap, bp, v1), 7)));
+            for (int i = 0; i < n; i++)
+            {
+                graph.AddEdgeToList(Tuple.Create(ap, points[i], Math.Round(Time(ap, points[i], v1), 7)));
+                graph.AddEdgeToList(Tuple.Create(bp, points[i], Math.Round(Time(bp, points[i], v1), 7)));
+            }
+            var dijk = graphAlgorithms.OrderedListShortesDistance(graph, ap, bp);
+            if (dijk.Count == 1)
+            {
+                Console.WriteLine("0.000000");
+                Console.WriteLine(0);
+                return;
+            }
+
+            List<int> pathList = new List<int>();
+
+            for (int i = 0; i < dijk.Count - 1; i++)
+            {
+                if (stationEdges.Contains(Tuple.Create(dijk[i].Item1, dijk[i + 1].Item1)) || stationEdges.Contains(Tuple.Create(dijk[i + 1].Item1, dijk[i].Item1)))
+                {
+                    var h = points.IndexOf(dijk[i].Item1) + 1;
+                    var f = points.IndexOf(dijk[i + 1].Item1) + 1;
+                    if (!pathList.Contains(h))
+                    {
+                        pathList.Add(h);
+                    }
+                    if (!pathList.Contains(f))
+                    {
+                        pathList.Add(f);
+                    }
+                }
+            }
+
+            Console.WriteLine($"{dijk.Last().Item2}");
+            Console.Write(pathList.Count + " ");
+            foreach (var item in pathList)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        public static double Time(KeyValuePair<double, double> a, KeyValuePair<double, double> b, double v)
+        {
+            var y0 = Math.Abs(b.Value - a.Value);
+            var x0 = Math.Abs(b.Key - a.Key);
+            var s = Math.Sqrt(x0 * x0 + y0 * y0);
+            return (double)(s / v);
+        }
+
+        static Dictionary<string, int> map = new Dictionary<string, int>();
+        static List<List<string>> groups = new List<List<string>>();
+        public static void Timus1208()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                groups.Add(new List<string>());
+                for (int l = 0; l < 3; l++)
+                {
+                    groups[i].Add(input[l]);
+                    map[groups[i][l]] = 0;
+                }
+            }
+
+            for (int i = 0; i < groups.Count; i++)
+            {
+                map[groups[i][0]] = 1;
+                map[groups[i][1]] = 1;
+                map[groups[i][2]] = 1;
+                Recursion(i + 1, 1);
+                map[groups[i][0]] = 0;
+                map[groups[i][1]] = 0;
+                map[groups[i][2]] = 0;
+            }
+
+
+            Console.WriteLine(max);
+        }
+
+        static int max = 0;
+        private static void Recursion(int index, int teamNum)
+        {
+            max = Math.Max(max, teamNum);
+            if (index < groups.Count)
+            {
+                if (map[groups[index][0]] == 0 && map[groups[index][1]] == 0 && map[groups[index][2]] == 0)
+                {
+                    map[groups[index][0]] = 1;
+                    map[groups[index][1]] = 1;
+                    map[groups[index][2]] = 1;
+                    Recursion(index + 1, teamNum + 1);
+                    map[groups[index][0]] = 0;
+                    map[groups[index][1]] = 0;
+                    map[groups[index][2]] = 0;
+                }
+                Recursion(index + 1, teamNum);
+            }
+        }
+
+        static void Timus1112()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var list = new List<Job>();
+            for (int i = 0; i < n; i++)
+            {
+                var se = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                list.Add(new Job(Math.Min(se[0], se[1]), Math.Max(se[1], se[0]), 1));
+            }
+            list = list.OrderBy(j => j.start).ToList();
+            list = list.OrderBy(j => j.finish).ToList();
+            int last = list[0].finish;
+            var res = new List<Tuple<int, int>>();
+            res.Add(Tuple.Create(list[0].start, list[0].finish));
+            for (int i = 1; i < n; i++)
+            {
+                if (list[i].start >= last)
+                {
+                    last = list[i].finish;
+                    res.Add(Tuple.Create(list[i].start, list[i].finish));
+                }
+            }
+            Console.WriteLine(res.Count);
+            foreach (var r in res)
+            {
+                Console.WriteLine(r.Item1 + " " + r.Item2);
+            }
+        }
+
+        public static void Timus1506()
+        {
+            var nk = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = nk[0];
+            var k = nk[1];
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            k = (n + k - 1) / k;
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = i; j < input.Length; j += k)
+                {
+                    if (j > input.Length) break;
+                    if (input[j] < 10)
+                        Console.Write("   " + input[j]);
+                    else if (input[j] < 100)
+                        Console.Write("  " + input[j]);
+                    else if (input[j] < 1000)
+                        Console.Write(" " + input[j]);
+                }
+                Console.WriteLine();
+            }
         }
 
     }
